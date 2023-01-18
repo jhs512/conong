0415d1fb1f80b836c582dcc4640b97f0bf6ed641
jhs512 <jangka512@gmail.com>
2023-01-18 17:56:46 +0900

    세팅

---
+++ src/main/java/com/ll/exam/conong/ConongApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.conong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConongApplication.class, args);
+    }
+
+}
