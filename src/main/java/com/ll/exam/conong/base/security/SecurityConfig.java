e90d3486dac24b086449ade6cc1faebcfa973b20
jhs512 <jangka512@gmail.com>
2023-01-19 23:21:15 +0900

    MethodSecurity 를 이용하여 로그인 상태에서 로그인 페이지에 접근할 수 없도록

--- src/main/java/com/ll/exam/conong/base/security/SecurityConfig.java
+++ src/main/java/com/ll/exam/conong/base/security/SecurityConfig.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 @Slf4j
 public class SecurityConfig {
--- src/main/java/com/ll/exam/conong/bounded_context/account/adapter/in/http/AccountController.java
+++ src/main/java/com/ll/exam/conong/bounded_context/account/adapter/in/http/AccountController.java
@@ -2,6 +2,7 @@ package com.ll.exam.conong.bounded_context.account.adapter.in.http;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @Slf4j
 public class AccountController {
+    @PreAuthorize("isAnonymous()")
     @GetMapping("/login")
     public String showLogin() {
         return "usr/account/login";
