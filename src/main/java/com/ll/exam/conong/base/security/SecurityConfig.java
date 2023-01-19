8aaa3f772d56163b1b6efeea5e9dc01fa59fa932
jhs512 <jangka512@gmail.com>
2023-01-19 14:42:22 +0900

    시큐리티 Config 을 추가하여 기본 보안설정 제거

---
+++ src/main/java/com/ll/exam/conong/base/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.ll.exam.conong.base.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+
+        return http.build();
+    }
+}
