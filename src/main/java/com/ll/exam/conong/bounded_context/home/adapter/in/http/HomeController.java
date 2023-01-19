b52d12322affa534b32fa68f0bbcb9a7d4abb735
jhs512 <jangka512@gmail.com>
2023-01-19 14:54:12 +0900

    DevTools 작동확인, LiveReload++ 크롬 익스텐션 설치 및 작동

--- src/main/java/com/ll/exam/conong/bounded_context/home/adapter/in/http/HomeController.java
+++ src/main/java/com/ll/exam/conong/bounded_context/home/adapter/in/http/HomeController.java
@@ -2,11 +2,18 @@ package com.ll.exam.conong.bounded_context.home.adapter.in.http;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String showHome() {
         return "usr/home/main";
+    }
+
+    @GetMapping("/version")
+    @ResponseBody
+    public int showVersion() {
+        return 15;
     }
 }
